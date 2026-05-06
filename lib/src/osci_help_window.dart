78cc9121d5aeb7b0d61e37096704c72c2e2a6d69
Winfried Klum <1108551+klumw@users.noreply.github.com>
2026-05-06 20:29:32 +0200

    fix: set IntrinsicColumnWidth for help window table to ensure proper cell sizing

--- lib/src/osci_help_window.dart
+++ lib/src/osci_help_window.dart
@@ -112,6 +112,7 @@ class _HelpWindowState extends State<HelpWindow> {
                               ),
                             ),
                           ),
+                          tableColumnWidth: const IntrinsicColumnWidth(),
                         ),
                   ),
           ),
