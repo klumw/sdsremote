d0971055274154c71ab93a145ed3a24548095e39
Winfried Klum <1108551+klumw@users.noreply.github.com>
2026-06-15 21:00:46 +0200

    feat: Bump version to 0.2.5 in configuration files and UI

--- lib/main.dart
+++ lib/main.dart
@@ -542,7 +542,7 @@ class _OsciHomePageState extends State<OsciHomePage>
                                       ),
                                       const SizedBox(height: 16),
                                       Text(
-                                        "Version 0.2.4",
+                                        "Version 0.2.5",
                                         style: TextStyle(
                                           color: Colors.white.withValues(
                                             alpha: 0.4,
